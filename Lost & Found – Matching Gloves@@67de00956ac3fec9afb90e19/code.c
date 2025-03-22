b6635da1f3d0d9528f5ae7b3d320337310c51740
samar <167969208+Samar0607@users.noreply.github.com>
2025-03-22 14:09:27 +0530

    Solution to Lost & Found – Matching Gloves

--- Lost & Found – Matching Gloves@@67de00956ac3fec9afb90e19/code.c
+++ Lost & Found – Matching Gloves@@67de00956ac3fec9afb90e19/code.c
@@ -6,11 +6,11 @@ int countGlovePairs(int gloves[],int n){
     for(int i=0;i<n;i++){
         for (int j=1;j<n;j++){
         if (gloves[i]==gloves[j]){
-            if (gloves[i]<=0&&gloves[j]<=0)
+            if (gloves[i]<=0&&gloves[j]<=0){
             count++;
             i++;
             gloves[j]=-1;
-        }
+        }}
         }
 
 return count;
