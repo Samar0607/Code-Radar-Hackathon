dfa5c9483b573107396722edda088fcc921abf19
samar <167969208+Samar0607@users.noreply.github.com>
2025-03-22 12:51:32 +0530

    Solution to Late Submission Penalty – Coding Contest

---
+++ Late Submission Penalty – Coding Contest@@67de00956ac3fec9afb90e16/code.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int calculatePenalty(int a ,int b,int c,int d,int e,int f){
+    if (c>f){
+        return 5000; 
+    }
+    else{
+        if (b>e){
+            return 200*(b-e);
+        }
+        else{
+            return 10*(a-d);
+        }
+
+    }
+}
